fda2e4d270f9f6172efd223e04c590a220cb83fc
MauriceTre <140593975+MauriceTre@users.noreply.github.com>
2024-05-07 08:57:59 +0200

    winston einbinden

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 const { Router } = require("express");
 const { AuthRouter } = require("./auth");
 const { UserRouter } = require("./users");
-const authMiddleWare = require("../middlewares/authMiddleware")
+const authMiddleWare = require("../middlewares/authMiddleware");
+const { Logger, log } = require("winston");
+const winston = require("winston");
 
 const AppRouter = Router();
 
 AppRouter.use("/auth", AuthRouter);
 // TODO: Die Users Router soll die auth Middleware durchlaufen
 AppRouter.use("/users", authMiddleWare, UserRouter);
+
 AppRouter.use("/users", UserRouter);
 
 module.exports = { AppRouter };
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { AppRouter } = require("./routes");
+const winston = require('winston');
 
 // Initialisierung von expres
 const app = express();
@@ -10,4 +11,27 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use("/v1", AppRouter);
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
 module.exports = app;
